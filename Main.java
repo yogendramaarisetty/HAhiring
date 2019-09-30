12a1a72b83c6842640f964f4c780099d30dad8be
yogi1001 <ymaarisetty@hostanalytics.com>
2019-09-30 11:21:13 +0530

    safsfaesf

--- Main.java
+++ Main.java
@@ -1,16 +1,6 @@
-import java.util.*;
 public class Main{
-	public static void main(String args[]){
-	Scanner scan = new Scanner(System.in);
-	int n=scan.nextInt();
-	System.out.println(fib(n));
-	}
-	static int fib(int n){
-	    if(n==1)
-	    return 1;
-	    else if(n==0)
-	    return 0;
-	    
-	    return fib(n-1)+fib(n-2);
-	}
+public static void main(String args[]){
+for(int i=0;i<10;i++)
+System.out.println("hello world");
+}
 }
